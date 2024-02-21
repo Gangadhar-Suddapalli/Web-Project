6caafbfb1bb38304f8a46195e84b630ce256abc0
ganga <ganga@192.168.0.204>
2024-02-21 09:51:50 +0530

    JEE Project

---
+++ JEE1/src/main/java/com/tap/Servlet1.java
@@ -0,0 +1,20 @@
+package com.tap;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class Servlet1 extends HttpServlet{
+	
+	@Override
+	protected void service(HttpServletRequest arg0, HttpServletResponse arg1) throws ServletException, IOException {
+		String un=arg0.getParameter("username");
+		PrintWriter out=arg1.getWriter();
+		out.print("<h1>Hello "+ un);
+	}
+
+}
